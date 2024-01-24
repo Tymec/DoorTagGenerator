f9a16ebfbd00bcda287bba9fcefd2fcb85fec7ae
Tymec <tymek1rt@hotmail.com>
2024-01-24 18:46:38 +0100

    Add icon, replace font size control for room members with slider

--- App/Views/StyleSettingsView.axaml.cs
+++ App/Views/StyleSettingsView.axaml.cs
@@ -6,12 +6,5 @@ namespace App.Views;
 public partial class StyleSettingsView : UserControl {
     public StyleSettingsView() {
         InitializeComponent();
-
-        foreach (var input in new[] {
-            RoomMembersSizeInput
-        }) {
-            input.KeyDown += NumericUpDownHandler.OnKeyDown;
-            input.PointerWheelChanged += NumericUpDownHandler.OnPointerWheelChanged;
-        }
     }
 }
